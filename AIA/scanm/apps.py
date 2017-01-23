b961398fecbc52d71cdc6f2c7ecd403996c7be30
dante993 <danteejmg@gmail.com>
2017-01-22 22:53:38 -0500

    correcion de nombres

--- AIA/scanm/apps.py
+++ AIA/scanm/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ScantitConfig(AppConfig):
-    name = 'scantit'
+class ScanmConfig(AppConfig):
+    name = 'scanm'
