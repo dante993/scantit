587835a67b72954b61f72bdcf0e446c96dd233d6
dante993 <danteejmg@gmail.com>
2017-01-23 07:14:47 -0500

    creacion de vista de inicio, url de inicio y uso de bootstrap

--- AIA/AIA/urls.py
+++ AIA/AIA/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from scanm.views import *
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', v_inicio, name='inicio'),
 ]
--- AIA/scanm/views.py
+++ AIA/scanm/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404,render_to_response
+from django.template import loader, context,RequestContext
+from django.http import *
 
 # Create your views here.
+def v_inicio(request):
+    mi_template = loader.get_template("index.html")
+    return HttpResponse(mi_template.render())
